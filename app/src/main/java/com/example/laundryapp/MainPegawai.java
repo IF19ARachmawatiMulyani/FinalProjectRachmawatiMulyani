e0d22b97a3e99e5c476bc402d3bf932a6a8a9862
OziFadhilah <ozifadhilah@gmail.com>
2021-07-12 16:30:22 +0700

    mengedit MainPegawai.java

--- app/src/main/java/com/example/laundryapp/MainPegawai.java
+++ app/src/main/java/com/example/laundryapp/MainPegawai.java
@@ -1,12 +1,21 @@
 package com.example.laundryapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
+
+//import android.support.annotation.NonNull;
+//import android.support.design.widget.BottomNavigationView;
+//import android.support.design.widget.CoordinatorLayout;
+//import android.support.v4.app.Fragment;
+//import android.support.v4.app.FragmentTransaction;
+//import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -14,8 +23,21 @@ public class MainPegawai extends AppCompatActivity {
     private Fragment fragment;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home_pegawai:
+                    fragment = new HomePegawaiFragment();
+                    loadFragment(fragment);
+                    return true;
+
+                case R.id.navigation_profile_pegawai:
+                    fragment = new ProfilePegawaiFragment();
+                    loadFragment(fragment);
+                    return true;
+            }
             return false;
         }
     };
@@ -24,6 +46,17 @@ public class MainPegawai extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_pegawai);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation_pegawai);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //Load the HomeFragment when app is loaded
+        fragment = new HomePegawaiFragment();
+        loadFragment(fragment);
+
+        //Hide the Bottom Navigation when the page is scrolled
+        CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) navigation.getLayoutParams();
+        layoutParams.setBehavior(new ScrollHandler());
     }
 
     private void loadFragment(Fragment fragment) {
@@ -32,4 +65,11 @@ public class MainPegawai extends AppCompatActivity {
         transaction.addToBackStack(null);
         transaction.commit();
     }
+
+    @Override
+    public void onBackPressed() {
+        //Close the application when back button is pressed
+        finish();
+    }
 }
+
