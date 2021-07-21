714959378e6f2f5e5addf334aaf3bdf66d9971c1
vian1211 <tetean1211@gmail.com>
2021-07-21 12:30:59 +0700

    Mengedit Register Pegawai

--- app/src/main/java/com/example/laundryapp/Register_pegawai.java
+++ app/src/main/java/com/example/laundryapp/Register_pegawai.java
@@ -1,24 +1,61 @@
 package com.example.laundryapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register_pegawai extends AppCompatActivity {
-
+    FirebaseAuth firebaseAuth;
+    EditText email, password, username;
+    Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_pegawai);
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseApp.initializeApp(this);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        username = findViewById(R.id.username);
+        register = findViewById(R.id.btn_login);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.createUserWithEmailAndPassword(email.getText().toString(),
+                        password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(Register_pegawai.this, "Registered successfully", Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(Register_pegawai.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
     }
+
     public void Register(View view) {
-        Intent register = new Intent(this, logIn.class);
+        Intent register = new Intent(this, FormLogin.class);
+        startActivity(register);
     }
 
     public void Login(View view) {
-        Intent login = new Intent(this, logIn.class);
+        Intent login = new Intent(this, FormLogin.class);
         startActivity(login);
     }
 }
